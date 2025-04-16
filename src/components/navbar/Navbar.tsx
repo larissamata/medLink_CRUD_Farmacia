0890c2a03d1a2b8d7d0bb65b75453154d4b94efe
Larissa <larissa.smata@hotmail.com>
2025-04-16 13:41:21 -0300

    Layout da página hoome, navbar e footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			bg-[#d6f2ee] text-[#006D77]'>
+            
+                <div className="container flex justify-between text-lg">
+                    MedLink
+
+                    <div className='flex gap-8'>
+                        Cadastrar Categorias
+
+                        Lista de Categorias
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+    return (
+        <>
+            <div className="bg-[#D6F2EE] flex justify-center items-center w-[100%] h-[80vh] overflow-hidden px-4 mx-auto">
+                <img
+                    src="https://ik.imagekit.io/larissamata/medlink2.png?updatedAt=1744815990590"
+                    alt="Imagem Página Home"
+                    className="max-h-[60vh] w-auto object-contain"
+                />
+            </div>
+        </>
+    );
+}
+
+export default Home;
